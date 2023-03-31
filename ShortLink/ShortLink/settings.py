130c0f8408d3fa4b676c85904d592d719da503e7
Anton Sliusar <63955212+lamertoxa@users.noreply.github.com>
2023-03-31 21:15:33 +0300

    remove DEBUG mode

--- ShortLink/ShortLink/settings.py
+++ ShortLink/ShortLink/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--(e5t_k)cn^2hnotj7v)t8x4uuyb=wa$)&ucz49c2v@+wxq!_%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 

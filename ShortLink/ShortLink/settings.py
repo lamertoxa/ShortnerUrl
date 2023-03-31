9e0f1b901ae752049991a1f4f9a36bb82b26b0b0
Anton Sliusar <63955212+lamertoxa@users.noreply.github.com>
2023-03-31 14:37:43 +0300

    add all host to access

--- ShortLink/ShortLink/settings.py
+++ ShortLink/ShortLink/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--(e5t_k)cn^2hnotj7v)t8x4uuyb=wa$)&ucz49c2v@+wxq!_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
